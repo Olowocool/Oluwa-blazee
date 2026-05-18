df219dcc5f2d89f143651642115d4e12f47ce4a6
Olowocool <mikerikltd@gmail.com>
2026-05-18 23:27:36 +0100

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -585,7 +585,8 @@ if data and "games" in data and len(data["games"]) > 0:
 
         game_home = normalize_team_name(game["home_team"]).lower()
         game_away = normalize_team_name(game["away_team"]).lower()
-
+    if not isinstance(odds_map, dict):
+        odds_map = {}
     for (home, away), value in odds_map.items():
             odds_home = normalize_team_name(home).lower()
             odds_away = normalize_team_name(away).lower()
