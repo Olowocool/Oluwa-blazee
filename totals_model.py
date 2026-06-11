2aaa403959d6ff403657a2640095fe31399a3a80
Olowocool <mikerikltd@gmail.com>
2026-06-11 21:43:52 +0100

    Update totals_model.py

--- totals_model.py
+++ totals_model.py
@@ -120,7 +120,10 @@ def get_team_recent_stats(history_df, team_name):
 
             rest_days = max(
                 0,
-                int((latest_game - previous_game).days) - 1
+                min(
+                    4,
+                    int((latest_game - previous_game).days) - 1
+                )
             )
         except Exception:
             rest_days = 1
